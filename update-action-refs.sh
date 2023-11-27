a5f762580d730923162bd2f3301bdbffc386cf7f
Ruud Senden <8635138+rsenden@users.noreply.github.com>
2023-11-27 12:25:51 +0100

    fix: test5

--- update-action-refs.sh
+++ update-action-refs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for f in $(find . -name 'action.yml'); do 
   grep -qrlZ 'uses: fortify/github-action/' $f && \
-    echo "Updating $f" && \
+    echo "Updating $f: version $1" && \
     sed -r -i -e "s|(uses: fortify\/github-action\/[^@]+)@.*|\1@$1|g" "$f"
 done
